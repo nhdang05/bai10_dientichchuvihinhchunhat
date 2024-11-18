fe3282a94b2ed2f90e387aa21fdb9d73eeeb6c15
nhdang05 <nguyenhaidang1162005@gmail.com>
2024-11-18 08:29:31 +0700

    Add project files.

---
+++ bai10_dientichchuvihinhchunhat/bai10_dientichchuvihinhchunhat.cpp
@@ -0,0 +1,20 @@
+// bai10_dientichchuvihinhchunhat.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "TINH DIEN TICH CHU VI HINH CHU NHAT\n";
+	double dai, rong, chuvi, dientich;
+	cout << " Nhap chieu dai:"; cin >> dai;
+	cout << "Nhap chieu rong:"; cin >> rong;
+
+	chuvi = (dai + rong) * 2;
+	dientich = dai * rong;
+	cout << "chu vi:" << chuvi << endl;
+	cout << "dien tich:" << dientich << endl;
+	return 0;
+		
+
+}
